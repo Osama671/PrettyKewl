d1a39eff6d7bec2c459c5b53ad5a039419f00506
Osama9991 <osamaazar48@hotmail.com>
2025-01-18 19:28:59 -0500

    .env setup uwu

--- server/express.ts
+++ server/express.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hey" });
