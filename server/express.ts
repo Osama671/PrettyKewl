21245eb39c50f016896ea92aaaa8654601eac4f6
Osama9991 <osamaazar48@hotmail.com>
2025-01-19 14:26:51 -0500

    Creating user/bcrypt

--- server/express.ts
+++ server/express.ts
@@ -1,9 +1,24 @@
 import express from "express";
 import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import createUser from "./repo/user.ts"
+
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(cors())
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+
+app.post("/aaa", (req, res) => {
+  console.log("Heya:", req.body)
+  const {username, password} = req.body
+  createUser(username, password)
+  res.json({message: "Success!"})
+})
 
 app.get("/", (req, res) => {
   res.json({ message: "Hey" });
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,13 +1,48 @@
-import { Link, useParams } from "react-router";
+import { FormEvent } from "react";
+import { Link } from "react-router";
+import axios from "axios";
 
 export default function Test() {
-  const { id } = useParams();
-  console.log(id);
+
+  const onLoginHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const onRegisterHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const [username, password] = e.target
+    console.log(username.value, password.value)
+    const response = await axios.post("/api/aaa", {
+      username: username.value,
+      password: password.value,
+    });
+  };
   return (
     <>
-      <div>HELLO</div>
+      <form onSubmit={onRegisterHandler}>
+        <h2> Register </h2>
+        <label htmlFor="username">Username: </label>
+        <input type="text" id="username" />
+        <br />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" />
+        <br />
+        <button>Login!</button>
+      </form>
+      <form onSubmit={onLoginHandler}>
+        <h2> Login </h2>
+        <label htmlFor="username2">Username: </label>
+        <input type="text" id="username2" />
+        <br />
+        <br />
+        <label htmlFor="password2">Password: </label>
+        <input type="text" id="password2" />
+        <br />
+        <button>Login!</button>
+      </form>
       <Link to="/">
-        <button>Return</button>
+        <button style={{ marginTop: "50px" }}>Return</button>
       </Link>
     </>
   );
