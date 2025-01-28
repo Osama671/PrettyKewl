26c2a9d083d5d97b89e8736a0bfc14d06f108cef
Osama9991 <osamaazar48@hotmail.com>
2025-01-28 13:35:01 -0500

    Carousel structure

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,22 +1,48 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-
-import "swiper/swiper-bundle.css"; // Main Swiper styles
+import { Box } from "@mui/material";
 
 export default function Carousel() {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100%",
+          backgroundImage: `url("../../tree.jpg")`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            height: "95%",
+            width: "100%",
+          }}
+        >
+          <Box sx={{ height: "100%", width: "8%", bgcolor: "blue" }}></Box>
+          <Box
+            sx={{
+              height: "100%",
+              width: "8%",
+              bgcolor: "blue",
+              marginLeft: "auto",
+            }}
+          ></Box>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            height: "5%",
+          }}
+        >
+          <Box sx={{ width: "100%", height: "100%", bgcolor: "green" }}></Box>
+        </Box>
+      </Box>
     </>
   );
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,5 @@
 import { Container, Grid2, Paper } from "@mui/material";
+import Carousel from "../components/Carousel";
 
 export default function Signup() {
   // const images = ["../../MainBefore.jpg", "../../tree.jpg"];
@@ -10,7 +11,7 @@ export default function Signup() {
         sx={{ width: "100%", height: "100vh", bgcolor: "red" }}
       >
         <Grid2 container spacing={3} columns={16} sx={{ height: "100%" }}>
-          <Grid2 size={9} sx={{ p: "64px", height: "100%" }}></Grid2>
+          <Grid2 size={9} sx={{ p: "64px", height: "100%" }}><Carousel></Carousel></Grid2>
           <Grid2 size={7} sx={{ p: "16px", height: "100%" }}>
             <Paper sx={{ height: "100%" }}>Hey</Paper>
           </Grid2>
