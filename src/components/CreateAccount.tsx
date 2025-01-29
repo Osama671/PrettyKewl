41d4b069934e1436b40c37aec80bcf9f91242210
Osama9991 <osamaazar48@hotmail.com>
2025-01-29 16:13:04 -0500

    Signup validation

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -6,10 +6,37 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { FormEvent } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Link } from "react-router";
+import { ValidateCreateAccount } from "../utils/formValidations";
 
 export default function CreateAccount() {
+  const [formFields, setFormFields] = useState({
+    username: "",
+    password: "",
+    password2: "",
+    email: "",
+  });
+
+  const [errors, setErrors] = useState({
+    username: "",
+    password: "",
+    password2: "",
+    email: "",
+  });
+
+  const handleFormValidation = () => {
+    setErrors(
+      ValidateCreateAccount(
+        formFields.username,
+        formFields.password,
+        formFields.password2,
+        formFields.email
+      )
+    );
+    console.log("Errors: ", errors);
+  };
+
   const onHandleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
@@ -17,9 +44,12 @@ export default function CreateAccount() {
     const password = formData.get("password");
     const password2 = formData.get("password2");
     const email = formData.get("email");
-    console.log(username, password, password2, email);
+    // console.log(username, password, password2, email);
   };
 
+  useEffect(() => {
+    handleFormValidation();
+  }, [formFields]);
   return (
     <>
       <Paper elevation={12} sx={{ height: "100%" }}>
@@ -37,34 +67,66 @@ export default function CreateAccount() {
             <FormControl fullWidth>
               <TextField
                 sx={{ mt: "4rem", alignSelf: "center", width: "65%" }}
+                error={errors.username !== ""}
+                helperText={`${errors.username || ""}`}
                 placeholder="Username"
                 name="username"
                 label="Username"
                 required
+                onChange={(e) => {
+                  setFormFields((oldValues) => ({
+                    ...oldValues,
+                    username: e.target.value,
+                  }));
+                }}
               ></TextField>
               <TextField
                 sx={{ mt: "1rem", alignSelf: "center", width: "65%" }}
                 placeholder="Password"
+                error={errors.password !== ""}
+                helperText={`${errors.password || ""}`}
                 type="password"
                 name="password"
                 label="Password"
                 required
+                onChange={(e) => {
+                  setFormFields((oldValues) => ({
+                    ...oldValues,
+                    password: e.target.value,
+                  }));
+                }}
               ></TextField>
               <TextField
                 sx={{ mt: "1rem", alignSelf: "center", width: "65%" }}
                 type="password"
                 placeholder="Re-enter password"
+                error={errors.password2 !== ""}
+                helperText={`${errors.password2 || ""}`}
                 name="password2"
                 label="Re-enter password"
                 required
+                onChange={(e) => {
+                  setFormFields((oldValues) => ({
+                    ...oldValues,
+                    password2: e.target.value,
+                  }));
+                }}
               ></TextField>
               <TextField
                 sx={{ mt: "1rem", alignSelf: "center", width: "65%" }}
                 type="email"
                 placeholder="Email"
+                error={errors.email !== ""}
+                helperText={`${errors.email || ""}`}
                 name="email"
                 label="Email"
                 required
+                onChange={(e) => {
+                  setFormFields((oldValues) => ({
+                    ...oldValues,
+                    email: e.target.value,
+                  }));
+                }}
               ></TextField>
               <Button
                 type="submit"
--- src/utils/formValidations.ts
+++ src/utils/formValidations.ts
@@ -1,10 +1,67 @@
-export const createAccountValidation = (
+export const ValidateCreateAccount = (
   username: string,
   password: string,
   password2: string,
   email: string
 ) => {
-    /*
+  const usernameRegex = /^[a-zA-Z0-9-_]{3,32}$/g;
+
+  // const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,100}$/;
+
+  const passwordUpperCaseRegex = /^(?=.*[A-Z]).+$/;
+  const passwordSpecialCharacterRegex = /^(?=.*[!@#$%^&*]).+$/;
+  const passwordOneNumberRegex = /^(?=.*\d).+$/;
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  const errors = { username: "", password: "", password2: "", email: "" };
+  if (username !== undefined) {
+    if (username.length === 0) {
+      errors.username = "";
+    } else if (username.length < 3) {
+      errors.username = "Username too short.";
+    } else if (username.length > 32) {
+      errors.username = "Username too long.";
+    } else if (usernameRegex.test(username) === false) {
+      errors.username = "Only special characters allowed are - and _.";
+    }
+  }
+
+  if (password !== undefined) {
+    if (password.length === 0) {
+      errors.password = "";
+    } else if (password.length < 8) {
+      errors.password = "Password too short! 8 minimum letters.";
+    } else if (password.length > 100) {
+      errors.password = "That's a long ass password. 100 max tyvm.";
+    } else if (passwordUpperCaseRegex.test(password) === false) {
+      errors.password = "Must have at least one uppercase character.";
+    } else if (passwordSpecialCharacterRegex.test(password) === false) {
+      errors.password = "Must have at least one special character.";
+    } else if (passwordOneNumberRegex.test(password) === false) {
+      errors.password = "Must hae at least one number.";
+    }
+  }
+
+  if (password2 !== undefined) {
+    if (password2.length === 0) {
+      errors.password2 = "";
+    } else if (password !== password2) {
+      errors.password2 = "Passwords do not match.";
+    }
+  }
+  if (email !== undefined) {
+    if (email.length === 0) {
+      errors.email = "";
+    } else if (emailRegex.test(email) === false) {
+      errors.email = "Not a valid email format.";
+    }
+  }
+
+  return errors;
+};
+
+/*
     Username validation:
     1- Characters >= 3 or <= 32
     2- only - and _ as special characters
@@ -20,9 +77,3 @@ export const createAccountValidation = (
     2- Domain must have letters and dots
     3- TLD must be between 2-6 characters
     */
-    const usernameRegex = /^[a-zA-Z0-9-_]{3,32}$/g
-
-    const passwordRegex = / /
-
-    const emailRegex = / /
-};
