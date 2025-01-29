d12caf147ac8a3059cf52b95756cae3940c7dc91
Osama9991 <osamaazar48@hotmail.com>
2025-01-29 17:47:34 -0500

    Backend validation better

--- server/middleware/validations.tsx
+++ server/middleware/validations.tsx
@@ -3,17 +3,26 @@ import { ValidateCreateAccount } from "../../shared/validations/formValidations"
 
 export const validateRegisterUser = async (
   req: Request,
-  _: Response,
+  res: Response,
   next: NextFunction
 ) => {
   try {
     const { username, password, password2, email } = req.body;
     const errors = ValidateCreateAccount(username, password, password2, email);
     if (Object.values(errors).some((ele) => ele !== "")) {
-      throw new Error(`Mismatched fields: ${errors}`);
+      function printErrors() {
+        Object.entries(errors).forEach(([key, value]) => {
+          const displayValue = value === "" ? "No Error" : value;
+          console.log(`${key}: ${displayValue} | VALUE: ${req.body[key]}`);
+        });
+      }
+      console.error(`------Mismatched fields------`)
+      console.error(printErrors())
+      res.status(401).json({message: "Validation Error"})
+      return
     }
     next();
   } catch (e) {
-    console.log(`Error in validateRegisterUser: ${e}`);
+    console.log(`Unknown error: ${e}`);
   }
 };
--- server/middleware/verifyJWT.ts
+++ server/middleware/verifyJWT.ts
@@ -5,26 +5,21 @@ import { NextFunction } from "express";
 
 dotenv.config();
 
-type DecodedExtendedOrSomeShit = {username: string}
-
+type DecodedExtended = { username: string };
 
 export const verifyJWT = (req: IRequestExtension, _, next: NextFunction) => {
   try {
     const token = req.cookies.token;
     if (!token) return next();
-    jwt.verify(
-      token,
-      process.env.JWTSECRET!,
-      function (err, decoded) {
-        if (err) {
-          console.log("Error: ", err.message);
-        }
-        if (decoded) {
-          const { username } = decoded as DecodedExtendedOrSomeShit
-          req.user = username;
-        }
+    jwt.verify(token, process.env.JWTSECRET!, function (err, decoded) {
+      if (err) {
+        console.log("Error: ", err.message);
       }
-    );
+      if (decoded) {
+        const { username } = decoded as DecodedExtended;
+        req.user = username;
+      }
+    });
     return next();
   } catch (e) {
     console.log(`Error in verifyJWT: ${e}`);
