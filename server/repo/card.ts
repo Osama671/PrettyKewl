41d6eba636c863c7725be72e1df5f913e25816d3
Osama9991 <osamaazar48@hotmail.com>
2025-02-01 01:01:45 -0500

    Seeding cards

---
+++ server/repo/card.ts
@@ -0,0 +1,26 @@
+import client from "./db";
+import { v4 as uuid } from "uuid";
+
+export async function createCard(quantity: number, price: number) {
+  try {
+    // const id = uuid();
+    const queryResult = await client.query(
+      "INSERT INTO card VALUES ($1, $2, $3)",
+      [uuid(), quantity, price]
+    );
+    console.log("Result: ", queryResult);
+    console.log("Card created!");
+    return queryResult;
+  } catch (e) {
+    console.error(`Error in DB: ${e}`);
+  }
+}
+
+export async function deleteAllCards() {
+  try {
+    const queryResult = await client.query("DELETE FROM card *");
+    return queryResult;
+  } catch (e) {
+    console.error(`Error in DB: ${e}`);
+  }
+}
